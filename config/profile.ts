78f316507d6f79c0778fac6d7a0c1c32e6546b04
KopaChaser <jj.joaopedro@outlook.com>
2025-01-03 13:01:44 -0300

    Update profile.ts

--- config/profile.ts
+++ config/profile.ts
@@ -5,17 +5,15 @@ export const profileConfig: ProfileConfig = {
     <script src="https://cdn.tailwindcss.com"></script>
     <body class="bg-gray-100 text-gray-800 font-sans flex flex-col items-center justify-center min-h-screen p-4">
 
-  <h1 id="title" class="text-4xl font-bold text-purple-500">Jovial</h1>
-  <p class="text-lg mt-2">"Coder" by hobby</p>
-  <p class="text-sm text-gray-500/60 line-through">or just jovi</p>
-  <p class="mt-2">I love gaming and messing with some codes sometimes</p>
-  <p class=" mt-4">
-    <a href="https://www.last.fm/user/YRangel1" class="text-blue-500 underline hover:text-blue-700">Last.fm profile</a>
-  </p>`,
-  lastfmUsername: "YRangel1",
-  avatar: "https://i.ibb.co/G0dWcBZ/Yajin-root.png",
+  <h1 id="title" class="text-4xl font-bold text-red-500">KopaChaser</h1>
+  <p class="text-sm text-gray-500/60 line-through">fvld slayer</p>
+  <p class="text-lg mt-2">Chill guy</p>
+  <p class="mt-2">Mono Shaco <br> 17y <br> discord: kopachaser</p>
+  `,
+  lastfmUsername: "KopaChaser",
+  avatar: "https://i.ibb.co/4sM4VbK/3e443f4caa45cce4632f02774c95ff68.jpg",
   wallpaper: {
-    url: "https://i.ibb.co/ThXg1ch/aesthetic-background-498-x-331-gif-tohuepdkx6vj8tkw.gif",
+    url: "https://i.ibb.co/fMg5D75/2394-moon-pfpsgg.gif",
     blur: {
       enabled: true,
       intensity: 10
@@ -25,26 +23,22 @@ export const profileConfig: ProfileConfig = {
   cardColor: "bg-gray-800/40",
   buttonOutlineColor: "gray-700/60",
   links: [
-    {
-      label: "GitHub",
-      url: "https://github.com/YRangel",
-      icon: "https://cdn-icons-png.flaticon.com/512/5968/5968866.png"
-    },
+    
     {
       label: "Twitter",
-      url: "https://twitter.com/schizokiss",
+      url: "https://x.com/KopaChaserSHACO",
       icon: "https://i.ibb.co/2yXsGWQ/Twitter-X-Rounded-Icon.png"
     },
     {
       label: "Last.fm",
-      url: "https://www.last.fm/user/YRangel1",
+      url: "https://www.last.fm/pt/user/KopaChaser",
       icon: "https://cdn-icons-png.flaticon.com/512/3291/3291646.png"
     }
   ],
   customLinks: [
     {
       label: "Discord <span class='text-blue-400'>&rarr;</span>",
-      url: "https://discord.gg/RqqddT8K",
+      url: "https://discord.gg/885rTkajzE",
       icon: "https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png"
     }
   ],
